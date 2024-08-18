fe7af133bfad278606af2c1cafd8069f76d1457c
KYs <amitkys59@gmail.com>
2024-08-18 09:17:28 +0530

    solve a math problem, added file in math directory

--- kys.cpp
+++ kys.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	string str = "amit";
-
-	for(auto ch : str){
-		cout << ch << " ";
+	// cout << "Hello World";
+	int x = 220;
+	int temp = x;
+	int ans = 0;
+	while(x){
+		int rem = x % 10;
+		if(rem == 0){
+			x = x / 10;
+			continue;
+		}
+		if(temp % rem == 0){
+			ans++;
+		}
+		x = x / 10;
 	}
+	cout << ans;
 }
