fd1ee090ea00628e8113c9d7822faecbbcb6d036
KYs <amitkys59@gmail.com>
2024-11-05 23:33:14 +0530

    first file is added

---
+++ Nov-24/5nov/linerSearch.cpp
@@ -0,0 +1,8 @@
+function bool search(int arr, int n, int x){
+	for(int i = 0; i < n; i++){
+		if(arr[i] == x){
+			return true;
+		}
+	}
+	return false;
+}
--- string/1st_str.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-	string name;
-	getline(cin, name);
-	cout << "Your name is: " << name << endl;
-	return 0;
-}
